331d16aac5e615d4bbe1ed2bf2846f90789d8afd
Cameron Hudson <CameronHudson8@gmail.com>
2021-10-23 20:23:55 -0700

    lint: lint error classes

--- src/errors/ResourceNotFoundError.ts
+++ src/errors/ResourceNotFoundError.ts
@@ -1,11 +1,9 @@
-import { HttpError } from "src/errors/HttpError";
+import { HttpError } from 'src/errors/HttpError'
 
 export class ResourceNotFoundError extends HttpError {
+  static readonly STATUS_CODE = 404;
 
-    static readonly STATUS_CODE = 404;
-
-    constructor(message: string) {
-        super(ResourceNotFoundError.STATUS_CODE, message || 'Resource not found');
-    }
-
+  constructor (message: string) {
+    super(ResourceNotFoundError.STATUS_CODE, message || 'Resource not found')
+  }
 }
