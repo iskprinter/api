63584bfc99c089536314edf557d3d2709cc7b310
Cameron Hudson <CameronHudson8@gmail.com>
2021-10-23 19:40:56 -0700

    refactor: move 500 error handler to middleware

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,24 @@ import express from 'express'
 import logger from 'morgan'
 
 import indexRoutes from 'src/routes/index'
+import { HttpError } from './errors/HttpError'
 
 const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(async (req, res, next) => {
+  try {
+    await next()
+  } catch (err) {
+    if (err instanceof HttpError) {
+      return res.status(err.statusCode).send(err.message)
+    }
+    console.error(err)
+    return res.sendStatus(500)
+  }
+})
 app.use('/', indexRoutes)
 
 export default app
--- src/routes/tokens.ts
+++ src/routes/tokens.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express'
 
 import { RequiredParams, RequestValidator } from 'src/tools/RequestValidator'
-import { HttpError } from 'src/errors/HttpError'
 import { AuthenticationController } from 'src/controllers/Authentication'
 import { Token } from 'src/entities/Token'
 import { TokenPostRequest, TokenVerificationResponse } from 'src/entities/TokenRequests'
@@ -10,48 +9,32 @@ import { BadRequestError } from 'src/errors/BadRequestError'
 const router = express.Router()
 
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    if (!req.headers.authorization && !req.headers.Authorization) {
-      throw new BadRequestError('Header \'authorization\' or \'Authorization\' is required.')
-    }
-    const authHeader = (req.headers.authorization || req.headers.Authorization) as string
-    if (authHeader === null) {
-      throw new BadRequestError('Header \'authorization\' or \'Authorization\' is required.')
-    }
-    const accessToken: string = (authHeader as any).match(/^Bearer (.*)$/)[1]
-
-    const tvr: TokenVerificationResponse = await (new AuthenticationController()).verifyToken(accessToken)
-    return res.json(tvr)
-  } catch (error) {
-    if (error instanceof HttpError) {
-      return res.status(error.statusCode).send(error.message)
-    }
-    console.error(error)
-    return res.sendStatus(500)
+  if (!req.headers.authorization && !req.headers.Authorization) {
+    throw new BadRequestError('Header \'authorization\' or \'Authorization\' is required.')
   }
+  const authHeader = (req.headers.authorization || req.headers.Authorization) as string
+  if (authHeader === null) {
+    throw new BadRequestError('Header \'authorization\' or \'Authorization\' is required.')
+  }
+  const accessToken: string = (authHeader as any).match(/^Bearer (.*)$/)[1]
+
+  const tvr: TokenVerificationResponse = await (new AuthenticationController()).verifyToken(accessToken)
+  return res.json(tvr)
 })
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const requiredParams: RequiredParams = {
-      body: [
-        'proofType',
-        'proof'
-      ],
-      query: []
-    };
-    (new RequestValidator(requiredParams)).validate(req)
+  const requiredParams: RequiredParams = {
+    body: [
+      'proofType',
+      'proof'
+    ],
+    query: []
+  };
+  (new RequestValidator(requiredParams)).validate(req)
 
-    const tokenRequest: TokenPostRequest = req.body
-    const token: Token = await (new AuthenticationController()).getToken(tokenRequest)
-    return res.json(token.accessToken)
-  } catch (error) {
-    if (error instanceof HttpError) {
-      return res.status(error.statusCode).send(error.message)
-    }
-    console.error(error)
-    return res.sendStatus(500)
-  }
+  const tokenRequest: TokenPostRequest = req.body
+  const token: Token = await (new AuthenticationController()).getToken(tokenRequest)
+  return res.json(token.accessToken)
 })
 
 export default router
